899d0f9d5e9e73c665b6f725a0e3c11f9d3416e1
go <gustaf@oxenstierna.org>
2023-04-20 21:11:05 +0200

    Use third party dependency with wrap

--- src/main.c
+++ src/main.c
@@ -1,6 +1,16 @@
+#include <cjson/cJSON.h>
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  printf("hello = %d\n", 1);
+  cJSON *json = cJSON_CreateObject();
+  if (!json) {
+    return 1;
+  }
+  if (!cJSON_AddStringToObject(json, "key", "value")) {
+    cJSON_Delete(json);
+    return 1;
+  }
+  printf("%s\n", cJSON_Print(json));
+  cJSON_Delete(json);
   return 0;
 }
